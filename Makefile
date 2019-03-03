377eb4f6f3947cfda2cb61af359705c73bfbfeaa
Hexwell <lazysloths@yahoo.com>
2019-03-03 23:11:12 +0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,16 @@
+GO_EASY_ON_ME = 1
+include $(THEOS)/makefiles/common.mk
+
+export TARGET = iphone:clang:11.2:11.0
+export ARCHS = arm64
+
+BUNDLE_NAME = vpnswitch
+vpnswitch_BUNDLE_EXTENSION = bundle
+vpnswitch_FILES = vpnswitch.m 
+vpnswitch_PRIVATE_FRAMEWORKS = ControlCenterUIKit
+vpnswitch_INSTALL_PATH = /Library/ControlCenter/Bundles/
+
+after-install::
+	install.exec "killall -9 SpringBoard"
+
+include $(THEOS_MAKE_PATH)/bundle.mk
---
+++ vpnswitch.m
@@ -0,0 +1,80 @@
+#import <dlfcn.h>
+#import <objc/runtime.h>
+#import <substrate.h>
+#import "vpnswitch.h"
+@interface VPNBundleController : NSObject
+-(id)initWithParentListController:(id)arg1 properties:(id)arg2 ;
+@end
+
+@implementation vpnswitch
+
+//Return the icon of your module here
+- (UIImage *)iconGlyph
+{
+	return [UIImage imageNamed:@"Icon" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
+}
+
+//Return the color selection color of your module here
+- (UIColor *)selectedColor
+{
+	return [UIColor blueColor];
+}
+
+- (BOOL)isSelected
+{
+  return _selected;
+}
+
+- (void)setSelected:(BOOL)selected
+{
+	_selected = selected;
+
+  [super refreshState];
+
+  if(_selected)
+  {
+		[self connectVPN];
+  }
+  else
+  {
+    [self disconnectVPN];
+  }
+}
+
+- (void)connectVPN{
+	NSBundle *VPNPreferences = [NSBundle bundleWithPath:@"/System/Library/PreferenceBundles/VPNPreferences.bundle"];
+	if([VPNPreferences load]==YES){
+		Class VPC = objc_getClass("VPNBundleController");
+			if(VPC!=NULL){
+					NSLog(@"VPNBundleController Loaded");
+					VPNBundleController *VPNBC =[[VPC alloc] initWithParentListController:nil];
+	  			if([VPNBC respondsToSelector:@selector(setVPNActive:)]){
+	    			[VPNBC setVPNActive:YES];
+	  			}
+	  else if([VPNBC respondsToSelector:@selector(_setVPNActive:)]){
+	      [VPNBC _setVPNActive:YES];
+	  }
+	[VPNBC release];
+	}
+	}
+}
+
+- (void)disconnectVPN{
+	NSBundle *VPNPreferences = [NSBundle bundleWithPath:@"/System/Library/PreferenceBundles/VPNPreferences.bundle"];
+	if([VPNPreferences load]==YES){
+		Class VPC = objc_getClass("VPNBundleController");
+			if(VPC!=NULL){
+					NSLog(@"VPNBundleController Loaded");
+					VPNBundleController *VPNBC =[[VPC alloc] initWithParentListController:nil];
+					if([VPNBC respondsToSelector:@selector(setVPNActive:)]){
+						[VPNBC setVPNActive:NO];
+					}
+		else if([VPNBC respondsToSelector:@selector(_setVPNActive:)]){
+				[VPNBC _setVPNActive:NO];
+		}
+	[VPNBC release];
+	}
+	}
+}
+
+@end
